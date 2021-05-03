9a7e0537d5b25812b0cf2a275c681ec5a0acf436
Patrycja-dz <patrycja.dziemianko@wp.pl>
2021-05-03 16:03:12 +0200

    created random functions for insults

--- script.js
+++ script.js
@@ -10,13 +10,16 @@
         "Granice? Nigdy żadnej nie widziałem, ale słyszałem, że istnieją w umysłach niektórych ludzi ~ Thor Heyerdahl",
         "Człowiek z no­wymi po­mysłami jest wa­riatem, dopóki nie od­niesie sukcesu. ~ Mark Twain"
     ];
+    const mathCalculations = (max) => {
+        return Math.floor(Math.random() * max)
+    }
     const getRandomQuote = () => {
-      return `${quotes[Math.floor(Math.random() * quotes.length)]}`
+        return `${quotes[Math.floor(Math.random() * quotes.length)]}`
 
     }
 
     const setRandomQuote = () => {
-        document.getElementById("js-generatorAnswer").innerText = getRandomQuote() ;
+        document.getElementById("js-generatorAnswer").innerText = getRandomQuote();
     };
 
     const onClickShowQuote = () => {
@@ -25,4 +28,21 @@
 
     };
     onClickShowQuote();
+
+    const randomBodyParts = ["Głowa", "Szyja", "Ręka", "Noga", "Brzuch", "Stopa", "Łydka"];
+    const randomAdjectives = ["cuchnąca", "duża", "brudna", "głupia", "mała", "okropna", "oślizgła", "zgnita"];
+    const randomAnimals = ["Mucha", "Małpa", "Słoń", "Wąż", "Glista", "Norka", "Skunks"];
+
+    const getRandomInsult = () => {
+        return `${randomBodyParts[Math.floor(Math.random() * randomBodyParts.length)]} ${randomAdjectives[Math.floor(Math.random() * randomAdjectives.length)]} jak ${randomAnimals[Math.floor(Math.random() * randomAnimals.length)
+        ]}`;
+    }
+    const setRandomInsult = () => {
+        document.getElementById("js-generatorAnswer").innerText = getRandomInsult();
+    }
+    const onClickShowInsult = () => {
+        const insultButton = document.querySelector(".js-insultButton");
+        insultButton.addEventListener("click", setRandomInsult);
+    }
+    onClickShowInsult()
 }
